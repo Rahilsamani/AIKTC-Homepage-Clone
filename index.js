23a7d4c7dd946dca21cc696304be163c17727c8a
Rahilsamani <rahilahmed1720@gmail.com>
2024-06-07 22:42:23 +0530

    add scroll animation

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && entry.target.classList.contains('hide')) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hide');
+
+hiddenElements.forEach((el) => observer.observe(el));
+
 const amountInput = document.querySelector('#amount');
 const name = document.querySelector('#name');
 const form = document.querySelector('form');
